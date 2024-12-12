239ed47d010efd410375c64988919eeb535ec06c
Golicshev <Golishev.uc@gmail.com>
2024-12-12 14:12:20 +0300

    Add files via upload

---
+++ Years.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string rubles = Console.ReadLine();
+
+                if (Convert.ToInt32(rubles) > -1 && Convert.ToInt32(rubles) < 1000)
+                    Console.WriteLine(Declension(Convert.ToUInt32(rubles)));
+            }
+        }
+
+        public static string Declension(uint rubles)
+        {
+            string ruble = rubles.ToString();
+            string rubleCase = "лет";
+            char lastChar = ruble[ruble.Length - 1];
+
+            if (ruble.Length >= 2 && ruble[ruble.Length - 2] == '1')
+            {
+                rubleCase = "лет";
+            }
+            else if (lastChar == '1')
+            {
+                rubleCase = "год";
+            }
+            else if (lastChar == '2' || lastChar == '3' || lastChar == '4')
+            {
+                rubleCase = "года";
+            }
+
+            return string.Format("{0} {1} ", rubles, rubleCase);
+        }
+    }
+}
